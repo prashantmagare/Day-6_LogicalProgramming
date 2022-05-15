12e894954807704b9f076e6203984f20a21e3db1
prashantmagare <prashantmagare3010@gmail.com>
2022-05-15 20:54:37 +0530

    Added: fibonacci Series Program

--- LogicalProgramming/LogicalProgramming/Program.cs
+++ LogicalProgramming/LogicalProgramming/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 
-namespace Day6LogicalPrograms
-{
-    internal class Program
+    namespace Day6LogicalPrograms
     {
-        static void Main(string[] args)
+        public class FibonocciNumbers
         {
-            Console.WriteLine("Welcome To Logical Programs");
+            int n1 = 0, n2 = 1, n3, number;
+
+            public void Fibo(int number)
+            {
+                Console.Write(n1 + " " + n2 + " ");//printing 0 and 1
+                for (int i = 2; i <= number; i++) //loop starts from 2 because 0 and 1 are already printed    
+                {
+                    if (i <= number)
+                    {
+                        n3 = n1 + n2;
+                        Console.Write(n3 + " ");
+                        n1 = n2;
+                        n2 = n3;
+                    }
+                }
+            }
+            public static void Main(string[] args)
+            {
+                Console.Write("Enter the number of elements: "); // Inpute
+                int number = Convert.ToInt32(Console.ReadLine());
+                FibonocciNumbers f = new FibonocciNumbers(); //Object
+                f.Fibo(number); //Call Method
+
+
+            }
         }
     }
-}
