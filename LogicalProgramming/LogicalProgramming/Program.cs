c028e958ad36cdc12d46e4bd88641add5d9a911b
prashantmagare <prashantmagare3010@gmail.com>
2022-05-15 20:34:34 +0530

    Welcome To Logical Programs

---
+++ LogicalProgramming/LogicalProgramming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day6LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Logical Programs");
+        }
+    }
+}
