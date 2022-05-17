39fda5ade1479380b1590513ee0781082ea2e11e
prashantmagare <prashantmagare3010@gmail.com>
2022-05-17 19:23:11 +0530

    Added: Perfect Number Program

--- LogicalProgramming/LogicalProgramming/Program.cs
+++ LogicalProgramming/LogicalProgramming/Program.cs
@@ -7,6 +7,29 @@ namespace Day6LogicalPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Logical Programs");
+
+            int number, sum = 0, n;
+            Console.Write("enter the Number \n");
+            number = int.Parse(Console.ReadLine());
+            n = number;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == n)
+            {
+                Console.WriteLine("\n Entered number is a perfect number");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("\n Entered number is not a perfect number");
+                Console.ReadLine();
+            }
         }
     }
 }
+    
