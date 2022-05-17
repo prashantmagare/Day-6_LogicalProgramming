09a3d4eaebae478175d81a7639c673092b7babbe
prashantmagare <prashantmagare3010@gmail.com>
2022-05-17 19:59:53 +0530

    Added: Prime Number Program

--- LogicalProgramming/LogicalProgramming/Program.cs
+++ LogicalProgramming/LogicalProgramming/Program.cs
@@ -2,11 +2,42 @@
 
 namespace Day6LogicalPrograms
 {
-    internal class Program
+
+
+    public class PrimeNumber
     {
-        static void Main(string[] args)
+        int m;
+        public void Prime(int number)
         {
-            Console.WriteLine("Welcome To Logical Programs");
+            m = number / 2;
+            for (int i = 2; i <= m; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.Write($" Given Number {number} is not Prime.");
+
+                }
+                else
+                {
+                    Console.WriteLine($"Given Number {number} is a Prime");
+                }
+            }
+
+        }
+
+
+    }
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            Console.Write("Enter the Number to check Prime: \n ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            PrimeNumber p = new PrimeNumber(); //object
+            p.Prime(number);
         }
     }
+
 }
