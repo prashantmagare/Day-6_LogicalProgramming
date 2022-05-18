7d16f3ad793ac816bc9d9cc2cf988ea31cfd9047
prashantmagare <prashantmagare3010@gmail.com>
2022-05-18 18:19:25 +0530

    Added: Reverse Number Program

--- LogicalProgramming/LogicalProgramming/Program.cs
+++ LogicalProgramming/LogicalProgramming/Program.cs
@@ -4,9 +4,35 @@ namespace Day6LogicalPrograms
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public class ReverseNumber
         {
-            Console.WriteLine("Welcome To Logical Programs");
+            int reverse = 0, rem;
+            public void Reverse_number(int number)
+            {
+                while (number != 0)
+                {
+                    rem = number % 10;
+                    reverse = reverse * 10 + rem;
+                    number /= 10;
+                }
+                Console.Write("Reversed Number: " + reverse);
+            }
         }
+
+        public class Reverse
+
+        {
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to Reverse Number Program");
+                Console.Write("Enter a number: \n ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                ReverseNumber r = new ReverseNumber();
+                r.Reverse_number(number);
+
+            }
+        }
+
     }
 }
+
