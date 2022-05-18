92022de8f9fa615e380710c4b7aa35ef5fdc1dbe
prashantmagare <prashantmagare3010@gmail.com>
2022-05-18 18:55:28 +0530

    Added: Coupon number program

--- LogicalProgramming/LogicalProgramming/Program.cs
+++ LogicalProgramming/LogicalProgramming/Program.cs
@@ -2,11 +2,43 @@
 
 namespace Day6LogicalPrograms
 {
-    internal class Program
+    internal class Coupon
     {
+        private string Generator()
+        {
+            string Code = "";
+            Code = Convert.ToString(new Random().Next(0,20));
+            return Code;
+        }
+        public Coupon()
+        {
+           
+            int N = Convert.ToInt32(Console.ReadLine());
+            string[] Coupon_List = new string[N];
+            string Coupon_Code = Generator();
+
+            for (int i = 0; i < N; i++)
+            {
+                while (Array.Exists(Coupon_List, element => element == Coupon_Code))
+                {
+                    Coupon_Code = Generator();
+                }
+                Coupon_List[i] = Coupon_Code;
+            }
+            Console.WriteLine("Required Coupon Numbers are, ");
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(Coupon_List[i]);
+            }
+        }
+
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Logical Programs");
+            Console.WriteLine("Welcome To Coupon Programs");
+            Console.WriteLine("Enter the number of unique coupons required (N): ");
+            Coupon coupon = new Coupon();
+
         }
     }
 }
